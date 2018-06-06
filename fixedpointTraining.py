2a9b27eaf3aae119ed5d16f73315a90fa69824de
wxrui5 <wxrui555@163.com>
2018-06-06 15:17:56 +0800

    finetune

--- fixedpointTraining.py
+++ fixedpointTraining.py
@@ -1,3 +1,4 @@
+import math
 import time
 import torch
 import torch.nn
@@ -61,16 +62,6 @@ def check_accuracy_part34(loader, model):
 
 
 def train_part34(model, optimizer, epochs=1):
-    """
-    Train a model on CIFAR-10 using the PyTorch Module API.
-
-    Inputs:
-    - model: A PyTorch Module giving the model to train.
-    - optimizer: An Optimizer object we will use to train the model
-    - epochs: (Optional) A Python integer giving the number of epochs to train for
-
-    Returns: Nothing, but prints model accuracies during training.
-    """
     model = model.to(device=config.device)  # move the model parameters to CPU/GPU
     if config.device.type == 'cuda':
         model = torch.nn.DataParallel(model)
@@ -136,3 +127,94 @@ class ExpConvNet(torch.nn.Module):
 model = ExpConvNet()
 optimizer = torch.optim.Adam(params=model.parameters(), lr=1e-3)
 train_part34(model, optimizer, epochs=5)
+
+torch.save(model.state_dict(), 'training.pt')
+
+
+class Quant:
+    def linear(input, bits):
+        assert bits >= 1, bits
+        if bits == 1:
+            return torch.sign(input) - 1
+        sf = torch.ceil(torch.log2(torch.max(torch.abs(input))))
+        delta = math.pow(2.0, -sf)
+        bound = math.pow(2.0, bits - 1)
+        min_val = - bound
+        max_val = bound - 1
+        rounded = torch.floor(input / delta)
+
+        clipped_value = torch.clamp(rounded, min_val, max_val) * delta
+        return clipped_value
+
+
+class quantization(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x, bits, quant_func):
+        # Define a constant
+        ctx.bits = bits
+        # ctx.save_for_backward(x)
+        clipped_value = quant_func(x, bits)
+        return clipped_value
+
+    def backward(ctx, grad_output):
+        grad_x = grad_output.clone()
+        return grad_x, None, None
+
+
+class activation_quantization(torch.nn.Module):
+    def __init__(self, bits=8, quant_func=Quant.linear):
+        super(activation_quantization, self).__init__()
+        self.bits = bits
+        self.func = quant_func
+
+    def forward(self, inputActivation):
+        return quantization.apply(inputActivation, self.bits, self.func)
+
+
+class FixedLayerConvNet(torch.nn.Module):
+    def __init__(self):
+        super(FixedLayerConvNet, self).__init__()
+        self.bits = 8
+        self.conv1 = torch.nn.Conv2d(3, 32, 5, stride=1, padding=2)
+        self.conv2 = torch.nn.Conv2d(32, 64, 3, stride=1, padding=1)
+        self.maxpool = torch.nn.MaxPool2d(kernel_size=2, stride=2)
+        self.bn1 = torch.nn.BatchNorm2d(32)
+        self.bn2 = torch.nn.BatchNorm2d(64)
+        self.fc1 = torch.nn.Linear(64 * 8 * 8, 512)
+        self.fc2 = torch.nn.Linear(512, 10)
+        self.quant = activation_quantization(8, Quant.linear)
+
+    def forward(self, x):
+        x = self.quant(x)
+        x = self.conv1(x)
+        x = torch.nn.functional.relu(x)
+        x = self.maxpool(torch.nn.functional.relu(x))
+        x = self.quant(x)
+        x = self.conv2(x)
+        x = torch.nn.functional.relu(x)
+        x = self.maxpool(torch.nn.functional.relu(x))
+        x = flatten(x)
+        x = self.quant(x)
+        x = self.fc1(x)
+        x = torch.nn.functional.relu(x)
+        x = self.quant(x)
+        x = self.fc2(x)
+        return x
+
+
+fix_model = FixedLayerConvNet()
+direct_fix_model = FixedLayerConvNet()
+PATH = 'training.pt'
+fix_model.load_state_dict(torch.load(PATH))
+direct_fix_model.load_state_dict(torch.load(PATH))
+
+learning_rate = 2e-6
+optimizer = torch.optim.Adam(params=fix_model.parameters(), lr=learning_rate)
+train_part34(fix_model, optimizer, epochs=10)
+
+print("Finetune Fixed Point Accuracy:")
+check_accuracy_part34(loader_test, fix_model)
+print("\nDirect Fixed Point Accuracy:")
+check_accuracy_part34(loader_test, direct_fix_model)
+print("\nOriginal Floating Point Accuracy:")
+check_accuracy_part34(loader_test, model)
